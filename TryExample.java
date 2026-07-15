b6aaf3c29a8f3b1d450ac9e0b5d74d993c6a52f4
2511cs030454-ctr <2511cs030454@mallareddyuniversity.ac.in>
2026-07-15 13:58:40 +0530

    Add files via upload

---
+++ TryExample.java
@@ -0,0 +1,13 @@
+public class TryExample{
+    public static void main(String[] args) {
+        try{
+            int a =10;
+            int b = 0;
+            int c = a/b;
+            System.out.println(c);
+        }catch(ArithmeticException e){
+        System.out.println("Cannot divide by Zero");
+        }
+        System.out.println("Programm continous");
+    }
+}
